608d1de9aca0c4dcd877e0ba34a143ff8f78dc29
Yusuf <yusuf@remtulla.com>
2026-09-07 12:09:53 +0000

    Use Platform enum for ClearSpace platforms

--- custom_components/clearspace/const.py
+++ custom_components/clearspace/const.py
@@ -1,7 +1,9 @@
 """Constants for the ClearSpace integration."""
 
+from homeassistant.const import Platform
+
 DOMAIN = "clearspace"
 CONF_API_TOKEN = "api_token"
 CONF_API_URL = "api_url"
 DEFAULT_API_URL = "https://clearspace-billing.yusuf-145.workers.dev"
-PLATFORMS = ["sensor"]
+PLATFORMS = [Platform.SENSOR]
